b5d162ec442dc891197f9eaf55d9904e8228f34e
Hitendra Kumar <138154413+Yashuu-ZX@users.noreply.github.com>
2023-09-05 09:33:45 +0530

    imporant_error_related_to_backslashn.c

--- imporant_error_related_to_backslashn.c
+++ imporant_error_related_to_backslashn.c
@@ -3,12 +3,12 @@ int main()
 {
     int a,b,c;
     char a1,b1,c1;
-    scanf("%d",&a);
-    scanf("%d",&b);
-    scanf("%d",&c);
-    scanf("%c",&a1);
-    scanf("%c",&b1);
-    scanf("%c",&c1);
+    scanf("%d",&a);//eg:- a = 1\n ---> a = 1
+    scanf("%d",&b);//eg:- b = /n2/n ---> b = 2
+    scanf("%d",&c);//eg:- c = /n3/n ---> c = 3
+    scanf("%c",&a1);//eg:- a1 = /na/n --> a1 = /n
+    scanf("%c",&b1);//eg:- a2 = a
+    scanf("%c",&c1);//eg:- a3 = /n
     printf("a = %d\nb = %d\nc = %d\n",a,b,c);
     printf("a1 = %u\nb1 = %u\nc1 = %u\n",a1,b1,c1);
     return 0;
