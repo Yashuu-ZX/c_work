e76ed456c2a6284b8da96cbac3e7ed4fff4eeef8
Hitendra Kumar <138154413+Yashuu-ZX@users.noreply.github.com>
2023-09-05 09:51:35 +0530

    Update imporant_error_related_to_backslashn.c

--- imporant_error_related_to_backslashn.c
+++ imporant_error_related_to_backslashn.c
@@ -4,7 +4,7 @@ int main()
     int a,b,c;
     char a1,b1,c1;
     scanf("%d",&a);//eg:- a = 1\n ---> a = 1
-    scanf("%d",&b);//eg:- b = /n2/n ---> b = 2
+    scanf("%d",&b);//eg:- b = /n2/n ---> b = 2 and /n before 2 is terminated coz it is not integer also sometimes last /n also gets terminated when no variable there
     scanf("%d",&c);//eg:- c = /n3/n ---> c = 3
     scanf("%c",&a1);//eg:- a1 = /na/n --> a1 = /0 ---> (/0 is null character)
     scanf("%c",&b1);//eg:- a2 = a
