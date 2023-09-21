1003545ec5a85751cb85ee6ec84eeb841659d3ce
Hitendra Kumar <138154413+Yashuu-ZX@users.noreply.github.com>
2023-09-21 16:50:12 +0530

    Add files via upload

---
+++ imp_ques.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+    for (int i = 5; i ; --i)
+    
+    {
+        printf("%d\n",i);
+    }
+    //any non-zero number in c means true so until i become zero it runs the code
+    return 0;
+}
